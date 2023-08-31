404b6a30de6f17926760fa15b3652de131b237d5
AnassShakeel <139561098+AnassShakeel@users.noreply.github.com>
2023-09-01 02:22:15 +0500

    Add files via upload

---
+++ javascipt 1 Assignment/app.js
@@ -0,0 +1,91 @@
+
+// Todo CHAPTER (1)
+
+//? Answer#1
+
+// alert("Assalamualaikum Warahmatullahi Wabarakatuh");
+
+//? Answer#2
+
+// alert("Error! Please enter a valid password");
+
+//? Answer#3
+
+// alert("Welcome to JS Land...\nHappy Coding!");
+
+//? Answer#4
+
+// alert("Welcome to JS Land...");
+
+// alert("Happy Coding!\n▢ Prevent this page from creating additional dialogs.");
+
+//? Answer#5
+
+// document.querySelector("body").innerHTML= "<h1> Yah! I can write HTML content through JavaScript."
+
+//! ##########
+
+// Todo CHAPTER (2)
+
+//? Answer#1
+
+// var userName;
+
+//? Answer#2
+
+// var myName;
+
+// myName = "Muhammad Anas";
+
+//? Answer#3
+
+// var message;
+
+// message = "Hello World";
+
+// alert(message);
+
+//? Answer#4
+
+// var studentName , studentAge , achievement ;
+
+// studentName = "Muhammad Anas"
+// studentAge = "20 year old"
+// achievement = "Web & App Development"
+
+// alert(studentName);
+// alert(studentAge);
+// alert(achievement);
+
+//? Answer#5 
+
+// var food;
+
+// food = "PIZZA \nPIZZ \nPIZ \nPI \nP";
+
+// alert(food);
+
+//? Answer#6
+
+// var email;
+
+// email = "Anasabbasi921@gmail.com";
+
+// alert("My email address is "+ email);
+
+//? Answer#7
+
+// var book;
+
+// book = "A smarter way to learn JavaScript";
+
+// alert("I am trying to learn from a Book "+book);
+
+//? Answer#8
+
+//  document.writeIn("Yah! I can write HTML content through JavaScript.");
+
+//? Answer#9
+
+// alert("▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬");
+     
